ebd192e699a7951eae9138e15a65fc85e8180c3e
田中裕也 <chop0522@icloud.com>
2025-05-26 00:39:01 +0900

    fix: set up @divination/astro-data workspace package

--- packages/astro-data/lib/index.ts
+++ packages/astro-data/lib/index.ts
@@ -1,2 +1,9 @@
-export * from './zodiac'
-export * from './numerology'
+export function getLifePathNumber(birth: string) {
+  // birth = "YYYY-MM-DD"
+  const digits = birth.replaceAll('-', '').split('').map(Number);
+  let sum = digits.reduce((a, b) => a + b, 0);
+  while (sum > 9 && ![11, 22, 33].includes(sum)) {
+    sum = String(sum).split('').reduce((a, b) => a + +b, 0);
+  }
+  return sum;
+}
